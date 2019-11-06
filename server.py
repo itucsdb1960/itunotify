9b1508466b52cffc3826cfe403ea1c6158b0ebec
AlpYilmazz <alperen0098@gmail.com>
2019-11-06 15:58:18 +0300

    added outlines for store and homepage

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
-
+import dbinit
+#import psycopg2
 
 app = Flask(__name__)
+connection_string = "dbname='postgres' user='postgres' password='' host='localhost' port=5432"
 
 
 @app.route("/")
@@ -11,3 +13,4 @@ def home_page():
 
 if __name__ == "__main__":
     app.run()
+    dbinit.initialize(connection_string)
