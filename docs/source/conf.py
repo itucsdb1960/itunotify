770d857f5be269cf54f20b4ee24c2c35984847e7
AlpEren Gençoğlu <alpgen99@hotmail.com>
2019-11-09 21:07:24 +0300

    changed conf.py file as requested

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,9 +19,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ITUCSDB18NN'
-copyright = '2018, ITUCSDB18NN'
-author = 'ITUCSDB18NN'
+project = 'ITUCSDB1960'
+copyright = '2019, ITUCSDB1960'
+author = 'ITUCSDB1960'
 
 # The short X.Y version
 version = ''
