eec50edf9cf2143848765b89f2c5b2f8430dcef1
AlpEren Gençoğlu <alpgen99@hotmail.com>
2019-11-09 23:01:41 +0300

    Lost & Found tables creation statements added

--- dbinit.py
+++ dbinit.py
@@ -7,6 +7,27 @@ import psycopg2 as dbapi2
 INIT_STATEMENTS = [
     "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
     "INSERT INTO DUMMY VALUES (42)",
+
+    """
+    CREATE TABLE IF NOT EXISTS lostfound (
+		postid SERIAL primary key,
+		title VARCHAR(32) NOT NULL,
+		description VARCHAR(512) NOT NULL,
+		userid INTEGER references user,
+		LF boolean NOT NULL,
+		location VARCHAR(32),
+		imageid INTEGER references image,
+	);""",
+
+	"""
+	CREATE TABLE IF NOT EXISTS responses (
+		respid SERIAL primary key,
+		postid INTEGER references lostfound,
+		response VARCHAR(512) NOT NULL,
+		userid INTEGER references user,
+		order integer NOT NULL
+	);"""
+
 ]
 
 
